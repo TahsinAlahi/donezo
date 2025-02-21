0215fcdc60b5d15749bc056d4659567f84714f10
Tahsin <alahitahsin@gmail.com>
2025-02-22 03:25:16 +0600

    frontend: made register page

--- Backend/app.js
+++ Backend/app.js
@@ -6,8 +6,8 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,42 @@
+import { Link } from "react-router-dom";
+import bgImage from "../../assets/homepage-bg.jpg";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div
+      className="w-screen h-[calc(100svh-52px)] bg-cover bg-right-bottom md:bg-left lg:bg-center"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <main className="max-w-screen-2xl h-full mx-auto flex items-center justify-center flex-col">
+        <div className="text-center space-y-5">
+          <h1 className="md:text-4xl text-2xl font-bold font-rubik">
+            Welcome to Donezo
+          </h1>
+          <p className="text-lg w-11/12 mx-auto px-3 font-poppins">
+            Drag, drop, and get things donezo—because productivity should be
+            fun, not a chore! 🎯💡
+            <br />
+            Stay organized, move fast, and watch your to-do list shrink in
+            style. ✅✨
+          </p>
+        </div>
+        <div className="space-x-3 text-lg mt-7">
+          <Link
+            to="/login"
+            className="font-semibold bg-navbar hover:text-white px-5 py-1 pb-2 rounded-lg transition duration-300 ease-in-out text-center"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="font-semibold bg-navbar hover:text-white px-5 py-1 pb-2 rounded-lg transition duration-300 ease-in-out text-center"
+          >
+            Sign up
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default HomePage;
